e316001796ea4fc92873f315a8e6ed65a900fb54
Cesar Segura <info@ticoworks.com>
2024-08-28 11:25:33 -0600

    retoques finales

--- galeria.php
+++ galeria.php
@@ -4,33 +4,28 @@
 
 <div class="galeria-principal">
     <div class="galeria-item">
-        <p>Titulo</p>
-        <img src="./images/FAUGET.png" alt="./index.php">
+        <p>Decoración de interiores</p>
+        <img src="./images/844f04fd4afb8a098abc3578608e4c7f.jpg" alt="./index.php">
     </div>
 
     <div class="galeria-item">
-        <p>Titulo</p>
-        <img src="./images/FAUGET.png" alt="./index.php">
+        <p>Decoración de Recámaras</p>
+        <img src="./images/3fefeffe68858505795d9a53da222d9e.jpg" alt="./index.php">
     </div>
 
     <div class="galeria-item">
-        <p>Titulo</p>
-        <img src="./images/FAUGET.png" alt="./index.php">
+        <p>Decoración de exteriores</p>
+        <img src="./images/0e60a26130b44e4104628759e6902129.jpg" alt="./index.php">
     </div>
 
     <div class="galeria-item">
-        <p>Titulo</p>
-        <img src="./images/FAUGET.png" alt="./index.php">
+        <p>Decoración de oficinas</p>
+        <img src="./images/0b023175d0f45aec900f032e895af4e2.jpg" alt="./index.php">
     </div>
 
     <div class="galeria-item">
-        <p>Titulo</p>
-        <img src="./images/FAUGET.png" alt="./index.php">
-    </div>
-
-    <div class="galeria-item">
-        <p>Titulo</p>
-        <img src="./images/FAUGET.png" alt="./index.php">
+     <p>Decoración de terrazas</p>
+        <img src="./images/2225117ced7ea2964ec1f75392c99ac0.jpg" alt="./index.php">
     </div>
 
 </div>
